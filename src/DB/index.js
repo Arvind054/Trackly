29426b4271f785188ca225dfd509c9390b3ba3df
Arvind <arvindchoudhary054@gmail.com>
2026-01-13 22:11:49 +0530

    DB and ORM setup

---
+++ src/DB/index.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import {drizzle} from "drizzle-orm/neon-http";
+import {neon} from "@neondatabase/serverless";
+
+
+// DB initialisation
+const sql = neon(process.env.DATABASE_URL);
+export const db = drizzle(sql);
