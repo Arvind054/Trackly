e8474ec6ca5b9645c47c8370a228bf9a7d34a916
Arvind <arvindchoudhary054@gmail.com>
2026-03-14 12:54:16 +0530

    Redis Caching For Live Users

--- app/api/live/route.tsx
+++ app/api/live/route.tsx
@@ -3,7 +3,7 @@ import { liveUserTable, pageViewsTable } from "@/src/DB/schema";
 import { and, eq ,gt} from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 import { UAParser } from "ua-parser-js";
-
+import { redis } from "@/lib/redis";
 const CORS_HEADERS = {
     'Access-Control-Allow-Origin':'*',
     'Access-Control-Allow-Methods':'GET,POST,OPTIONS',
@@ -21,57 +21,9 @@ export async function OPTIONS(req:Request){
 export async function POST(req: NextRequest){
 try {
     const body = await req.json();
-    const { visitorId, websiteId, last_seen, url } = body;
-
-    // 1) Parse device info from User-Agent
-    const parser = new UAParser(req.headers.get("user-agent") || "");
-    const deviceInfo = parser.getDevice()?.model || "";
-    const osInfo = parser.getOS()?.name || "";
-    const browserInfo = parser.getBrowser()?.name || "";
-
-    // 2) Get IP from headers (or fallback)
-    const ip =
-      req.headers.get("x-forwarded-for")?.split(",")[0] ||
-      req.headers.get("x-real-ip") ||
-      "71.71.22.54"; // fallback IP
-
-    // 3) Fetch geo-location info from IP
-    const geoRes = await fetch(`http://ip-api.com/json/${ip}`);
-    const geoInfo = await geoRes.json();
-
-    // 4) Upsert into liveUser table
-     await db
-      .insert(liveUserTable)
-      .values({
-        visitorId,
-        websiteId,
-        last_seen,
-        city: geoInfo.city || "",
-        region: geoInfo.regionName || "",
-        country: geoInfo.country || "",
-        countryCode: geoInfo.countryCode || "",
-        lat: geoInfo.lat?.toString() || "",
-        lng: geoInfo.lon?.toString() || "",
-        device: deviceInfo,
-        os: osInfo,
-        browser: browserInfo,
-      })
-      .onConflictDoUpdate({
-        target: liveUserTable.visitorId,
-        set: {
-          last_seen,
-          city: geoInfo.city || "",
-          region: geoInfo.regionName || "",
-          country: geoInfo.country || "",
-          countryCode: geoInfo.countryCode || "",
-          lat: geoInfo.lat?.toString() || "",
-          lng: geoInfo.lon?.toString() || "",
-          device: deviceInfo,
-          os: osInfo,
-          browser: browserInfo,
-        },
-      });
-     
+    const { visitorId, websiteId,} = body;
+      // Add to Redis
+      await redis.zadd(`live:${websiteId}`,Date.now(),visitorId);
     return NextResponse.json({ status: "ok" },{headers:CORS_HEADERS});
   } catch (err: any) {
     console.error(err);
@@ -85,16 +37,9 @@ try {
 export async function GET(req: NextRequest){
      const websiteId = req.nextUrl.searchParams.get("websiteId");
   const now = Date.now();
-
-  const activeUsers = await db
-    .select()
-    .from(liveUserTable)
-    .where(
-      and(
-        gt(liveUserTable.last_seen, String(now - 30_000)),
-        eq(liveUserTable.websiteId, websiteId as string)
-      )
-    );
-
+  const key = `live:${websiteId}`;
+  await redis.zremrangebyscore(key, 0, now - 30000);
+  const activeUsers = await redis.zcard(key);
+  console.log("active users are ", activeUsers);
   return NextResponse.json(activeUsers);
 }
